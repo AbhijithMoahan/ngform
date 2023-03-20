e1fe1535068f762c4029aba494bcb018e0685c6f
Abhijith Mohanan <abhijithmohan404@iclod.com>
2023-03-20 16:37:26 +0200

    created the form without the styling with validations

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent {
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      jobPosition: [''],
+      email: ['', Validators.email]
+    });
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      window.alert('Please fill in the madadotory fields ');
+      return;
+    }
+    console.log(this.form.value);
+  }
 
 }
